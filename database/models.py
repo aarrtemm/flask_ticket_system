08139413d1389d09c35ffbb9f7042561473fe5f3
aarrtemm <getapple@MacBook-Pro.local>
2024-05-26 14:11:54 +0300

    Created models: User, Group, Ticket

---
+++ database/models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(
+        db.Enum("Admin", "User", "Manager", "Analyst"),
+        nullable=False,
+        default="User"
+        )
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
+
+
+
+class Group(db.Model):
+    id  = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    users = db.relationship("User", backref="group")
+
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(
+        db.Enum("Pending", "Closed", "In review"), default="Pending", nullable=False
+        )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
