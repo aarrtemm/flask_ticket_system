9257bbf80b07ca88e473dc9b8931d580e8887ef2
aarrtemm <getapple@MacBook-Pro.local>
2024-05-30 16:28:56 +0300

    Fixed problems

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SelectField
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SelectField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
 
+from database.models import User
 
 
 class RegisterForm(FlaskForm):
@@ -9,6 +10,11 @@ class RegisterForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired(), Length(min=3, max=20)])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username.")
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
